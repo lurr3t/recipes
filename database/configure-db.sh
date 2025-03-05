8f8db206825a82794cf145b0471b0f32a6238e20
ludwigfallstrom <dv20lfm@cs.umu.e>
2025-03-05 17:19:22 +0100

    Changed db image to work with apple silicon

--- database/configure-db.sh
+++ database/configure-db.sh
@@ -6,22 +6,29 @@
 # https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017 
 
 
+# Wait for SQL Server to start
 DBSTATUS=1
 ERRCODE=1
-i=0
-
-while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 10 ]] && [[ $ERRCODE -ne 0 ]]; do
-	i=$i+1
-	DBSTATUS=$(/opt/mssql-tools/bin/sqlcmd -h -1 -t 1 -U sa -P $MSSQL_SA_PASSWORD -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases")
-	ERRCODE=$?
-	sleep 1
+while [ $DBSTATUS -ne 0 ] || [ $ERRCODE -ne 0 ]; do
+    sleep 1
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "SELECT 1" &> /dev/null
+    DBSTATUS=$?
+    ERRCODE=$?
 done
 
-if [ $DBSTATUS -ne 0 ] OR [ $ERRCODE -ne 0 ]; then 
-	echo "SQL Server took more than 60 seconds to start up or one or more databases are not in an ONLINE state"
-	exit 1
+if [ $DBSTATUS -ne 0 ] || [ $ERRCODE -ne 0 ]; then 
+    echo "SQL Server took more than 60 seconds to start up or one or more databases are not in an ONLINE state"
+    exit 1
 fi
 
-# Run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "CREATE DATABASE projekt"
+# Retry mechanism for database creation
+RETRY_COUNT=5
+RETRY_INTERVAL=5
+for i in $(seq 1 $RETRY_COUNT); do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "CREATE DATABASE projekt" && break
+    echo "Retrying database creation in $RETRY_INTERVAL seconds..."
+    sleep $RETRY_INTERVAL
+done
+
+# Run the setup script to create the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d projekt -i sql_scripts/create.sql
--- database/run.sh
+++ database/run.sh
@@ -1,3 +1,3 @@
 source ../.env
 
-docker run --rm -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD' -p 1433:1433 --name sql1 -it mssql-custom
+docker run --rm -e "ACCEPT_EULA=1" -e "MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD" -e "MSSQL_PID=Developer" -e "MSSQL_USER=sa" -p 1433:1433 --name=sql1 -it mssql-custom
